383169b4854bb53be4a6db962820e43e8386c665
dandi122 <skykjk122@naver.com>
2024-09-19 17:30:50 +0900

    feat:2024-09-19

---
+++ backend_1/src/main/java/com/example/demo/controller/ChatbotController.java
@@ -0,0 +1,14 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatbotController {
+	
+	@GetMapping("/chatbot")
+	public String home() {
+		return "chatbot";
+	}
+	
+}
